2c8e4e86102e554dd9c5fd60eac0ea70a76c3f1d
Joseph Won <godspeedyoo@gmail.com>
2015-04-13 16:55:30 -0700

    add example app controller, module, and sample values to scope

--- app/assets/javascripts/angular-app/app.js
+++ app/assets/javascripts/angular-app/app.js
@@ -1,9 +1,17 @@
-app = angular.module('app', []);
+'use strict';
 
-app.config(['$httpProvider', function($httpProvider) {
-    $httpProvider.defaults.headers.common['X-CSRF-TOKEN'] = $('meta[name=csrf-token]').attr('content')
+var app = angular.module('app', [
+  'ngRoute',
+  'appControllers',
+  'appFilters',
+  'appServices'
+]);
+
+app.config(['$httpProvider', 
+  function($httpProvider) {
+      $httpProvider.defaults.headers.common['X-CSRF-TOKEN'] = $('meta[name=csrf-token]').attr('content')
 }]);
 
 app.run(function() {
-    console.log('sc2tracker app running');   
+  console.log('sc2tracker app running');   
 })
