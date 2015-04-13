a433c763359129ba034309097681368e7f2b38e8
Joseph Won <godspeedyoo@gmail.com>
2015-04-13 16:34:16 -0700

    Add angular assets to asset pipeline

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,14 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require_tree .
+//= require angular
+//= require angular-rails-templates
+
+//= require angular-app
+//= require_tree ./angular-app/templates
+//= require_tree ./angular-app/modules
+//= require_tree ./angular-app/filters
+//= require_tree ./angular-app/directives
+//= require_tree ./angular-app/models
+//= require_tree ./angular-app/services
+//= require_tree ./angular-app/controllers
