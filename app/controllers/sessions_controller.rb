9048bcd48c2d10c90b16f703bf734259cda5d221
Joseph Won <godspeedyoo@gmail.com>
2015-04-14 16:37:41 -0700

    add HTTParty gem; store access_token in session; need to refactor to helper methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,11 +6,16 @@ class SessionsController < ApplicationController
 
   def create
     puts "*"*50
-    puts "Hit #Create route"
+    puts "PARAMS:"
+    puts params
+    puts session
     auth = request.env["omniauth.auth"]
+
     session[:user_id] = auth["uid"]
     session[:user_info] = auth["info"]
-    session[:access_token] = 
+    session[:access_token] = auth.credentials.token
+    response = HTTParty.get('https://us.api.battle.net/sc2/profile/user?access_token=' + session[:access_token]) 
+    byebug
     redirect_to OmniAuth.config.full_host
   end
 
