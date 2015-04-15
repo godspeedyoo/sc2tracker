9f3afdd9fdf7e82d0d82fe5ffd70b710a1275245
Joseph Won <godspeedyoo@gmail.com>
2015-04-14 17:07:34 -0700

    put response back into welcome controller temporarily; add auth_logout path and link to welcome view

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    
+    # remove after profile has been moved to its own page
+    @response = HTTParty.get('https://us.api.battle.net/sc2/profile/user?access_token=' + session[:access_token])
   end
 end
