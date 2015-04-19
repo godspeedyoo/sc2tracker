fd28b3b681ed0c8e3cbc80e7af2110a1e743e819
Joseph Won <godspeedyoo@gmail.com>
2015-04-18 17:00:17 -0700

    Modify profile view to show scope variables of profile, i.e. name, id

--- app/assets/javascripts/angular-app/controllers/profile-controller.js
+++ app/assets/javascripts/angular-app/controllers/profile-controller.js
@@ -1,11 +1,10 @@
 angular.module('app', ["appServices"])
 .controller("ProfileIndexCtrl", ['$http','$scope','ProfileFactory',
   function($http, $scope, ProfileFactory) {
-    
     ProfileFactory.getProfile().then(function(response) {
-      $scope.profile = response.data;
-      // debugger;
-      console.log(response.data);
+      $scope.profile = response.data.characters[0];
+      $scope.name = $scope.profile.name
+      $scope.id = $scope.profile.id
     });
   }
 
--- app/assets/javascripts/angular-app/modules/sc2Module.js
+++
@@ -1,7 +0,0 @@
-var app = angular
-    .module('app',[
-]);
-
-app.run(function() {
-    console.log('sc2 running');
-});  
